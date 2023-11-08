bf58f8fc1bb538cae1f38d7a73beeb5dc0838d61
olgatorres <bestlaly@mail.ru>
2023-11-08 18:49:15 +0300

    added pics

---
+++ Arcitecture/Seminar_7/static/script.js
@@ -0,0 +1,27 @@
+(async function f() {
+    async function fetchAndRender() {
+        const path = window.location.pathname.split("/");
+        const page = path[path.length - 1] || "main";
+        try {
+            const response = await fetch(`/get_page_html/${page}`);
+            if (response.ok) {
+                document.getElementById("main-page").innerHTML = await response.text();
+            } else {
+                console.error("Error response");
+            }
+        } catch (error) {
+            console.error("Error response", error);
+        }
+    }
+    document.querySelectorAll(".menu-link").forEach(function (element) {
+        element.addEventListener("click", async function (event) {
+            event.preventDefault();
+            history.pushState(null, null, element.id !== "main" ? `/${element.id}`: "/");
+
+            await fetchAndRender();
+        });
+    });
+
+    window.onpopstate = fetchAndRender;
+    await fetchAndRender();
+})();
